2576c953d7f333f15e414844bfb27791caa79ac1
jorg-j <jorgensen.server@gmail.com>
2025-08-09 07:46:15 +1000

    add abbrv

--- init.lua
+++ init.lua
@@ -8,7 +8,7 @@ vim.g.maplocalleader = ' '
 vim.opt.number = true
 -- You can also add relative line numbers, to help with jumping.
 --  Experiment for yourself to see if you like it!
--- vim.opt.relativenumber = true
+vim.opt.relativenumber = true
 
 -- Enable mouse mode, can be useful for resizing splits for example!
 vim.opt.mouse = 'a'
@@ -25,7 +25,10 @@ require("remap")
 --vim.cmd('iabbrev @@greeting Hello There!<CR>This is a pre-written message<CR>Thank you')
 
 vim.keymap.set("n", "<Leader>o", "iOLED_CLEAR<CR>", { noremap = true, silent = true })
+vim.keymap.set("n", "<Leader>c", ':let @/ = ""<CR>', { desc = 'Clear search highlights' })
+vim.keymap.set("n", "<Leader>\\", ':luafile $MYVIMRC', { desc = 'Reload luafile' })
 
 
 vim.cmd([[iabbrev __greeting Hello there<CR>This is a test]])
 vim.cmd([[iabbrev dOLED OLED_CLEAR<CR>OLED_CURSOR 0 0<CR>OLED_PRINT]])
+vim.cmd([[iabbrev ilog logger.info(f"")<LEFT><LEFT>]])
