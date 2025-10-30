68698b1e3f3d5a035b225df2222697947acccb65
Jack <jack.jorgensen@avecglobal.com>
2025-10-30 12:47:53 +1100

    lsp

--- init.lua
+++ init.lua
@@ -89,4 +89,17 @@ vim.api.nvim_create_autocmd('DirChanged', {
 })
 
 
+-- lsp
+local lsp_zero = require('lsp-zero')
 
+lsp_zero.on_attach(function(client, bufnr)
+  lsp_zero.default_keymaps({buffer = bufnr})
+end)
+
+require('mason').setup({})
+require('mason-lspconfig').setup({
+  ensure_installed = {'pylsp', 'ruff'},
+  handlers = {
+    lsp_zero.default_setup,
+  },
+})
--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -1,21 +1,15 @@
-	return {
-  {
-    "neovim/nvim-lspconfig",
-    opts = {
-      servers = {
-        pyright = {
-          settings = {
-            python = {
-              analysis = {
-                autoSearchPaths = true,
-                diagnosticMode = "workspace",
-                useLibraryCodeForTypes = true,
-                typeCheckingMode = "basic"  -- or "strict"
-              }
-            }
-          }
-        }
-      }
-    }
+return {
+  'VonHeikemen/lsp-zero.nvim',
+  branch = 'v3.x',
+  dependencies = {
+    -- LSP Support
+    {'neovim/nvim-lspconfig'},
+    {'williamboman/mason.nvim'},
+    {'williamboman/mason-lspconfig.nvim'},
+
+    -- Autocompletion
+    {'hrsh7th/nvim-cmp'},
+    {'hrsh7th/cmp-nvim-lsp'},
+    {'L3MON4D3/LuaSnip'},
   }
 }
