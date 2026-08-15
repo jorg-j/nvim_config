ffd0163bcbb62f7e0ec26c4c316ae0ddd50d2895
Jack Jorgensen <jorgensen.jack@gmail.com>
2026-08-15 18:50:47 +1000

    add inserts

---
+++ lua/jack/inserts.lua
@@ -0,0 +1,10 @@
+vim.cmd([[
+function! ClientLetter() abort
+  let name = input('Client name: ')
+  return "Dear " . name . ",\n\n"
+        \ . "Thank you for choosing to work with " . name . ". "
+        \ . "We at Acme Corp look forward to supporting " . name . "'s goals..."
+endfunction
+
+iabbrev <expr> ,clientletter ClientLetter()
+]])
--- lua/jack/remap.lua
+++ lua/jack/remap.lua
@@ -9,4 +9,20 @@ vim.keymap.set("n", "<ENTER>", "o<ESC>", { desc = 'Add empty line', noremap = tr
 vim.keymap.set("n", "<S-ENTER>", "O<ESC>", { desc = 'Add empty line before', noremap = true, silent = true })
 vim.keymap.set("n", "fj", "<ESC>", { desc = 'Add empty line before', noremap = true, silent = true })
 
+vim.keymap.set('t', '<Esc>', [[<C-\><C-n>]], { desc = 'Exit term mode', noremap = true, silent = true })
 
+
+
+vim.opt.viewoptions:remove("curdir") -- avoid clobbering cwd on load, optional
+
+local augroup = vim.api.nvim_create_augroup("AutoSaveFolds", { clear = true })
+vim.api.nvim_create_autocmd("BufWinLeave", {
+  pattern = "*",
+  group = augroup,
+  command = "silent! mkview",
+})
+vim.api.nvim_create_autocmd("BufWinEnter", {
+  pattern = "*",
+  group = augroup,
+  command = "silent! loadview",
+})
