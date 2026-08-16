32c0ac7cd25af4beb2a7ebc3514b437f53bbf437
Jack Jorgensen <jorgensen.jack@gmail.com>
2026-08-16 12:25:02 +1000

    update inserts

--- lua/jack/inserts.lua
+++ lua/jack/inserts.lua
@@ -1,10 +1,11 @@
+
 vim.cmd([[
 function! ClientLetter() abort
   let name = input('Client name: ')
-  return "Dear " . name . ",\n\n"
+  return "Dear " . name . ",\r\r"
         \ . "Thank you for choosing to work with " . name . ". "
         \ . "We at Acme Corp look forward to supporting " . name . "'s goals..."
 endfunction
 
-iabbrev <expr> ,clientletter ClientLetter()
+iabbrev <expr> clientletter ClientLetter()
 ]])
